aa0adddf541ba8c6735076f017c7a4acc00933f7
natemaia <natemaia10@gmail.com>
2018-02-25 18:17:56 -0800

    Large cleanup and fixing of installer

--- archlabs/build.sh
+++ archlabs/build.sh
@@ -80,7 +80,7 @@ make_setup_mkinitcpio() {
     local _hook
     mkdir -p "$work_dir/$arch/airootfs/etc/initcpio/hooks"
     mkdir -p "$work_dir/$arch/airootfs/etc/initcpio/install"
-    for _hook in archiso archiso_shutdown archiso_pxe_common archiso_pxe_nbd archiso_pxe_http archiso_pxe_nfs archiso_loop_mnt; do
+    for _hook in archiso archiso_shutdown archiso_loop_mnt; do
         cp /usr/lib/initcpio/hooks/$_hook "$work_dir/$arch/airootfs/etc/initcpio/hooks"
         cp /usr/lib/initcpio/install/$_hook "$work_dir/$arch/airootfs/etc/initcpio/install"
     done
@@ -88,7 +88,6 @@ make_setup_mkinitcpio() {
     cp /usr/lib/initcpio/install/archiso_kms "$work_dir/$arch/airootfs/etc/initcpio/install"
     cp /usr/lib/initcpio/archiso_shutdown "$work_dir/$arch/airootfs/etc/initcpio"
     cp "$script_path/mkinitcpio.conf" "$work_dir/$arch/airootfs/etc/mkinitcpio-archiso.conf"
-    # gnupg_fd=
     if [[ $gpg_key ]]; then
       gpg --export $gpg_key >$work_dir/gpgkey
       exec 17<>$work_dir/gpgkey
