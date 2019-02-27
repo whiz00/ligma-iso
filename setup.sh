07d4f93a327b36390ea7b27c1ff619224e301acd
Gabriel Hansson <gabrielhansson00@gmail.com>
2019-02-27 13:28:07 +0100

    working setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dir="/home/$1/ligma-iso"
 mkdir -pv $dir/build
-sudo rsync -av $dir/* $dir/build/ --exclude $dir/build
+sudo rsync -av $dir/* $dir/build/ --exclude $dir/build README.md setup.sh
 pacman -Scc
