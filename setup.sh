5759e79d8e05b25e669211777d537af319ef272a
Gabriel Hansson <gabrielhansson00@gmail.com>
2019-02-27 13:22:12 +0100

    setup test

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 dir="/home/$1/ligma-iso"
 mkdir -pv $dir/build
-sudo cp -r $dir $dir/build
-sudo rsync -av --progress $dir $dir/build --exclude $dir/build
+sudo rsync -av $dir/* $dir/build/ --exclude $dir/build
 pacman -Scc
